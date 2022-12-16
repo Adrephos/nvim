0c13d61948e14140a669a617c1bd30d38f379ad8
Fer L <afriguez@gmail.com>
2022-12-15 23:59:48 -0500

    feat: add nvim_tree plugin config

---
+++ lua/plugins/config/nvim_tree.lua
@@ -0,0 +1,26 @@
+require("nvim-tree").setup({
+  sort_by = "extension",
+  git = {
+	enable = true,
+	ignore = false,
+  },
+  view = {
+    adaptive_size = true,
+	float = {
+	  enable = true,
+	  quit_on_focus_loss = true,
+	  open_win_config = {
+		relative = "cursor",
+		height = 15,
+	  }
+	},
+  },
+  renderer = {
+    group_empty = true,
+	icons = {
+	  show = {
+		git = true,
+	  },
+	},
+  },
+})
