19dcc99642fb3bcb41b1471df3b9651b1bbc545c
Fer L <afriguez@gmail.com>
2022-12-15 23:57:45 -0500

    feat: add plugins (packer)

---
+++ lua/plugins/init.lua
@@ -0,0 +1,43 @@
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local repository = 'https://github.com/wbthomason/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = vim.fn.system({'git', 'clone', '--depth', '1', repository, install_path})
+end
+
+local ok, packer = pcall(require, "packer")
+if not ok then return end
+
+require('plugins.config.nvim_tree')
+require('plugins.config.wilder')
+require('plugins.config.hop')
+require('plugins.config.lualine')
+
+return require('packer').startup(function(use) 
+  use 'tpope/vim-fugitive'
+  use 'jiangmiao/auto-pairs'
+  use 'mattn/emmet-vim'
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'ThePrimeagen/harpoon'
+  use 'phaazon/hop.nvim'
+  use 'gelguy/wilder.nvim'
+
+  use 'elixir-editors/vim-elixir'
+  use 'mhinz/vim-mix-format'
+  use 'yuezk/vim-js'
+  use 'HerringtonDarkholme/yats.vim'
+  use 'MaxMEllon/vim-jsx-pretty'
+
+  use 'afriguez/dracula.nvim'
+
+  use {
+	'nvim-lualine/lualine.nvim',
+	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-tree/nvim-tree.lua'
+  use 'rcarriga/nvim-notify'
+
+  if PACKER_BOOTSTRAP then require('packer').sync() end
+end)
