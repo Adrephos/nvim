ed11fba4183dfa5c343c02a39b4d1035be9bfd5d
Fer L <afriguez@gmail.com>
2022-12-16 11:50:10 -0500

    feat: add on_attach to mason

--- lua/plugins/config/mason.lua
+++ lua/plugins/config/mason.lua
@@ -8,10 +8,24 @@ require('mason').setup({
 	}
 })
 
+local on_attach = function(_, bufnr)
+	local opts = { noremap=true, silent=true }
+
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gd', '<cmd>lua vim.lsp.buf.definitions()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gp', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+end
+
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers {
 	function (server_name)
-		require('lspconfig')[server_name].setup {}
+		require('lspconfig')[server_name].setup {
+			on_attach = on_attach
+		}
 	end
 }
