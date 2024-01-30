6782035d0d6edd90fd8f72ba1df0da7d23921d41
Adrephos <adrephos@gmail.com>
2024-01-30 18:12:10 -0500

    feat: add mapping for vim.lsp.buf.rename and vim.lsp.buf.code_action

--- lua/plugins/config/mason.lua
+++ lua/plugins/config/mason.lua
@@ -1,19 +1,21 @@
 require('mason').setup({
 	ui = {
 		icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
 	}
 })
 
 local on_attach = function(_, bufnr)
-	local opts = { noremap=true, silent=true }
+	local opts = { noremap = true, silent = true }
 
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>gp', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
@@ -23,7 +25,7 @@ end
 local mason_lspconfig = require('mason-lspconfig')
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers {
-	function (server_name)
+	function(server_name)
 		require('lspconfig')[server_name].setup {
 			on_attach = on_attach
 		}
