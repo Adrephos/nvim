272d3e6098e16d96caf0036657f0a43fae687047
Juan A. <adrephos@gmail.com>
2025-05-30 12:21:55 -0500

    feat: change tab character

--- lua/plugins/config/indent_blankline.lua
+++ lua/plugins/config/indent_blankline.lua
@@ -1,3 +1,18 @@
+local highlight = {
+  "ScopeGray",
+}
+
+local hooks = require "ibl.hooks"
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, "ScopeGray", { fg = "#a0a0B0" })
+end)
+
 require("ibl").setup({
-  scope = { enabled = true },
+  scope = {
+    enabled = true,
+    highlight = highlight
+  },
+  indent = {
+    tab_char = "▍",
+  }
 })
